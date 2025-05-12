389f4c729b4ad144d6ffac48b778d83f5438a4b0
Prashely <danielprashely@gmail.com>
2025-05-12 10:28:12 +0200

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
