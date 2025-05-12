402ebe54068f9b42d185036c0a460145c1ab7ccf
Prashely Maluleke <danielprashely@gmail.com>
2025-05-12 10:22:16 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
+ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
