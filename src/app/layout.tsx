8fd4b789205d3a110a81c8da7d312c1637a70a24
Prashely <danielprashely@gmail.com>
2025-05-12 10:41:53 +0200

    removed daRKMODE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       */}
       <head />
 
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <body className={`bg-[#FCFCFC] ${inter.className}`}>
         <Providers>
           <Header />
           {children}
@@ -34,4 +34,3 @@ export default function RootLayout({
 }
 
 import { Providers } from "./providers";
-
